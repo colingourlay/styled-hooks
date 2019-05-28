c6340c61dce1f79c849f632183198928e502ca48
Colin Gourlay <Gourlay.Colin@abc.net.au>
2019-05-28 15:19:11 +1000

    Remove unit resolution feature.

--- src/css.ts
+++ src/css.ts
@@ -1,7 +1,5 @@
 import Stylis from '@emotion/stylis';
 
-const UNITS_PATTERN = /^(cap|ch|em|ex|ic|lh|rem|rlh|vh|vw|vmin|vmax|px|cm|mm|Q|in|pc|pt|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx|x|%)/g;
-
 const stylis = new Stylis();
 
 const customPropsRule = (className: string, customProps: {}) =>
@@ -28,34 +26,15 @@ export function generateCSSWithCustomProps(
   let sharedInput = '';
   let customProps = {};
   let nextCustomPropIndex = 0;
-  let editedNextString = '';
 
   for (let i = 0, len = strings.length; i < len; i++) {
-    if (editedNextString) {
-      sharedInput += editedNextString;
-      editedNextString = '';
-    } else {
-      sharedInput += strings[i];
-    }
+    sharedInput += strings[i];
 
     if (values[i] != null) {
       const customPropKey = `--${sharedClassName}-${nextCustomPropIndex++}`;
 
       sharedInput += `var(${customPropKey})`;
       customProps[customPropKey] = String(values[i]);
-
-      if (strings[i + 1]) {
-        const [unit] = strings[i + 1].match(UNITS_PATTERN) || [''];
-
-        if (unit) {
-          if (!customProps[customPropKey].length) {
-            customProps[customPropKey] = '0';
-          }
-
-          customProps[customPropKey] += unit;
-          editedNextString = strings[i + 1].slice(unit.length);
-        }
-      }
     }
   }
 
